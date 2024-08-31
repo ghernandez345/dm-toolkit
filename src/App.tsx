c87a7ad7ed286ce2a8dba35e8739fedf59f0d786
Gabriel Hernandez <ghernandez345@gmail.com>
2024-08-31 09:48:02 +0100

    starting add pc

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
 import { getPlayerCharacters } from "./entities/playerCharacter";
+import NewPCForm from "./NewPCForm/NewPCForm";
 
 import PlayerCharacterCard from "./PlayerCharacterCard/PlayerCharacterCard";
+
+const ActionsButton = () => {
+  // TODO: fix the plus charaters alignment
+
+  const onClick = () => {};
+
+  return (
+    <button
+      className="absolute bottom-4 rounded-full bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-400 active:bg-blue-600"
+      onClick={onClick}
+    >
+      <span>&#43;</span>
+    </button>
+  );
+};
 
 function App() {
   const testCharacters = getPlayerCharacters();
 
   return (
-    <div className="p-6">
+    <div className="min-h-screen p-6">
       <>
         {testCharacters.map((playerCharacter) => (
           <PlayerCharacterCard
@@ -15,6 +31,9 @@ function App() {
           />
         ))}
       </>
+
+      <NewPCForm />
+      <ActionsButton />
     </div>
   );
 }
---
+++ src/NewPCForm/NewPCForm.tsx
@@ -0,0 +1,64 @@
+import { useForm, SubmitHandler } from "react-hook-form";
+
+interface NewPCFromFields {
+  name: string;
+  race: string;
+  class: string;
+  level: number;
+  hp: number;
+  ac: number;
+  passivePerception: number;
+  passiveInvestigation: number;
+  passiveInsight: number;
+}
+
+interface NewPCFormProps {}
+
+const NewPCForm = ({}: NewPCFormProps) => {
+  const { register, handleSubmit } = useForm<NewPCFromFields>();
+
+  const onSubmit: SubmitHandler<NewPCFromFields> = (data) => {
+    console.log("submit", data);
+  };
+
+  return (
+    <form className="flex flex-col" onSubmit={handleSubmit(onSubmit)}>
+      <label htmlFor="name">Character Name</label>
+      <input type="text" id="name" {...register("name")} />
+      <label htmlFor="race">Race</label>
+      <input type="text" id="race" {...register("race")} />
+      <label htmlFor="class">Class</label>
+      <input type="text" id="class" {...register("class")} />
+      <label htmlFor="level">Level</label>
+      <input type="text" id="level" {...register("level")} />
+      <label htmlFor="hp">HP</label>
+      <input type="number" id="hp" {...register("hp")} />
+      <label htmlFor="ac">AC</label>
+      <input type="number" id="ac" {...register("ac")} />
+      <fieldset>
+        <legend>Passive Skills</legend>
+        <label htmlFor="passivePerception">Perception</label>
+        <input
+          type="number"
+          id="passivePerception"
+          {...register("passivePerception")}
+        />
+        <label htmlFor="passiveInvestigation">Investigation</label>
+        <input
+          type="number"
+          id="passiveInvestigation"
+          {...register("passiveInvestigation")}
+        />
+        <label htmlFor="passiveInsight">Insight</label>
+        <input
+          type="number"
+          id="passiveInsight"
+          {...register("passiveInsight")}
+        />
+      </fieldset>
+      <button type="submit">Create</button>
+    </form>
+  );
+};
+
+export default NewPCForm;
